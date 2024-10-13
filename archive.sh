6c55d7b0f6b0e885ddfc5834d20d16f1a8524074
Jeff Irwin <jirwin505@gmail.com>
2024-10-13 15:03:10 -0400

    shader toy 2024-10-14 (preemptive)

--- archive.sh
+++ archive.sh
@@ -3,9 +3,15 @@
 set -exu
 
 today=$(date +"%F")
+#today="2024-10-14"
 
 archive_dir="archive/$today"
 mkdir -p "$archive_dir"
 
-cp *.c *.h *.glsl "$archive_dir"
+files=$(               \
+	ls *.c *.h *.glsl  \
+	|| true            \
+)
+
+cp $files "$archive_dir"
 
